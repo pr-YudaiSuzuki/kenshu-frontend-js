32e3450b86f25cdaf8c082202acad8d990fa25d6
yudai.suzuki <yudai.suzuki@prtimes.co.jp>
2020-04-27 12:16:11 +0900

    finish 05

---
+++ practice/05/try/src/js/index.js
@@ -0,0 +1,10 @@
+(() => {
+  'use strict';
+
+  const input = document.getElementById('input');
+
+  input.addEventListener('input', () => {
+    const text = input.value.indexOf('yes') > -1 ? input.value : '';
+    document.getElementById('text').textContent = text;
+  });
+});
